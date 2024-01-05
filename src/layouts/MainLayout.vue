3310a1410434551e0279cd10704e30fccf9060f0
Aleksey <rusal@bk.ru>
2024-01-05 22:56:35 +0300

    add old

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,10 +12,10 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          Информационно-аналитическая система поиска и анализа активных эксплоитов в корпоративных диалоговых текстах
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+
       </q-toolbar>
     </q-header>
 
